4f8360a4ee8e2d9a73c0638a70ed749b67062258
Alexander-Lukyanov <46135664+Alexander-Lukyanov@users.noreply.github.com>
2020-05-21 13:36:28 +0300

    Головна функція програми

---
+++ main.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+from funckmu import *
+from basic_func import *
+import csv
+import os
+
+
+def main():
+    profayl_posl()
+    # napovnennya_posl()
+
+
+
+if __name__ == '__main__':
+    main()
+ 
